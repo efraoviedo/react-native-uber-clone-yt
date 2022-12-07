020d51d6155624e3b5cb1711c289fab1829cbc54
efranor <efraoviedo@gmail.com>
2022-12-07 11:36:42 -0500

    react native app

--- App.js
+++ App.js
@@ -1,20 +1,45 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import "react-native-gesture-handler";
+import React from "react";
+import { KeyboardAvoidingView, Platform } from "react-native";
+import { Provider } from "react-redux";
+import HomeScreen from "./screens/HomeScreen";
+import MapScreen from "./screens/MapScreen";
+import { store } from "./store";
+import { SafeAreaProvider } from "react-native-safe-area-context";
+import { NavigationContainer } from "@react-navigation/native";
+import { createStackNavigator } from "@react-navigation/stack";
 
 export default function App() {
+  const Stack = createStackNavigator();
+
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
+    <Provider store={store}>
+      <NavigationContainer>
+        <SafeAreaProvider>
+          <KeyboardAvoidingView
+            behavior={Platform.OS === "ios" ? "padding" : "height"}
+            style={{ flex: 1 }}
+            keyboardVerticalOffset={Platform.OS === "ios" ? -64 : 0}
+          >
+            <Stack.Navigator>
+              <Stack.Screen
+                name="HomeScreen"
+                component={HomeScreen}
+                options={{
+                  headerShown: false,
+                }}
+              />
+              <Stack.Screen
+                name="MapScreen"
+                component={MapScreen}
+                options={{
+                  headerShown: false,
+                }}
+              />
+            </Stack.Navigator>
+          </KeyboardAvoidingView>
+        </SafeAreaProvider>
+      </NavigationContainer>
+    </Provider>
   );
 }
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
---
+++ components/RideOptionsCard.js
@@ -0,0 +1,115 @@
+import React, { useState } from "react";
+import {
+  FlatList,
+  SafeAreaView,
+  StyleSheet,
+  Text,
+  TouchableOpacity,
+  View,
+} from "react-native";
+import tw from "tailwind-react-native-classnames";
+import { Icon } from "react-native-elements";
+import { useNavigation } from "@react-navigation/native";
+import { Image } from "react-native";
+import { useSelector } from "react-redux";
+import { selectTravelTimeInformation } from "../slices/navSlice";
+import "intl";
+import "intl/locale-data/jsonp/en";
+
+const data = [
+  {
+    id: "Uber-X-123",
+    title: "UberX",
+    multiplier: 1,
+    image: "https://links.papareact.com/3pn",
+  },
+  {
+    id: "Uber-LUX-789",
+    title: "Uber LUX",
+    multiplier: 1.75,
+    image: "https://links.papareact.com/7pf",
+  },
+  {
+    id: "Uber-XL-456",
+    title: "Uber XL",
+    multiplier: 1.2,
+    image: "https://links.papareact.com/5w8",
+  },
+];
+// If we have  SURGE pricing, this goes up
+const SURGE_CHARGE_RATE = 1.5;
+
+const RideOptionsCard = () => {
+  const navigation = useNavigation();
+  const [selected, setSelected] = useState(null);
+  const travelTimeInformation = useSelector(selectTravelTimeInformation);
+
+  return (
+    <SafeAreaView style={tw`bg-white flex-grow`}>
+      <View>
+        <TouchableOpacity
+          onPress={() => navigation.navigate("NavigateCard")}
+          style={tw`absolute top-3 left-5  z-50 p-3 rounded-full`}
+        >
+          <Icon name="chevron-left" type="font-awesome" />
+        </TouchableOpacity>
+        <Text style={tw`text-center py-5 text-xl`}>
+          Select a Ride -{travelTimeInformation?.distance?.text}
+        </Text>
+      </View>
+      <FlatList
+        data={data}
+        keyExtractor={(item) => item.id}
+        renderItem={({ item: { id, title, multiplier, image }, item }) => (
+          <TouchableOpacity
+            onPress={() => setSelected(item)}
+            style={tw`flex-row justify-between items-center px-10 
+            ${id === selected?.id && "bg-gray-200"}`}
+          >
+            <Image
+              style={{
+                width: 100,
+                height: 100,
+                resizeMode: "contain",
+              }}
+              source={{ uri: image }}
+            />
+
+            <View style={tw`-ml-8`}>
+              <Text style={tw`text-sm font-semibold`}>{title}</Text>
+              <Text style={tw`text-xs`}>
+                {travelTimeInformation?.duration?.text}Travel Time
+              </Text>
+            </View>
+            <Text style={tw`text-sm`}>
+              {new Intl.NumberFormat("en-gb", {
+                style: "currency",
+                currency: "GBP",
+              }).format(
+                (travelTimeInformation?.duration?.value *
+                  SURGE_CHARGE_RATE *
+                  multiplier) /
+                  100
+              )}
+            </Text>
+          </TouchableOpacity>
+        )}
+      />
+
+      <View style={tw`mt-auto border-t border-gray-200`}>
+        <TouchableOpacity
+          disabled={!selected}
+          style={tw`bg-black py-3 m-3 ${!selected && "bg-gray-300"}`}
+        >
+          <Text style={tw`text-center text-white text-xl`}>
+            Choose {selected?.title}
+          </Text>
+        </TouchableOpacity>
+      </View>
+    </SafeAreaView>
+  );
+};
+
+export default RideOptionsCard;
+
+const styles = StyleSheet.create({});
